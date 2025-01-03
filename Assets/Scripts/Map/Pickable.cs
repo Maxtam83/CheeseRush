60ff8877a7bbaa0f2b2412123defd9a040714213
Maxime TAMARIN <maxtam83390@gmail.com>
2025-01-03 16:19:23 +0100

    [AMELIORATION] prise en charge du trophée et de la clé dans le script Pickable.cs

--- Assets/Scripts/Map/Pickable.cs
+++ Assets/Scripts/Map/Pickable.cs
@@ -1,16 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Pickable : MonoBehaviour
 {
-    [SerializeField] private string playerTag = "Player"; // Le tag du joueur
+    [SerializeField] private string playerTag   = "Player"; // Le tag du joueur
+    [SerializeField] private string TrophyName   = "Trophy"; // Le tag du Trophée
+    [SerializeField] private string KeyName      = "Key";    // Le tag de la clé
 
     private void OnTriggerEnter(Collider other)
     {
         // Vérifie si l'objet entrant dans le trigger a le tag "Player"
         if (other.CompareTag(playerTag))
         {
-            // Ajoute une pièce au compteur global via le GameManager
-            GameManager.Instance.AddCoin();
+            // cas ou on récupère le trophée
+            if (gameObject.name == TrophyName)
+                // action fin
+                Debug.Log("TODO : Faire action de fin quand trophée récupéré");
+            
+            // cas ou on récupère la clé
+            else if (gameObject.name == KeyName)
+                Debug.Log("TODO : Faire action quand récupération de la clé");
+
+            // cas ou on récupère une pièce
+            else
+                // Ajoute une pièce au compteur global via le GameManager
+                GameManager.Instance.AddCoin();
 
             // Détruit cet objet (celui auquel ce script est attaché)
             Destroy(gameObject);
